68ba0a59c03ee178b443b21f1ddc4c263274e323
WA-FRAMEXTEAM <jayaweeraduneesha@gmail.com>
2025-05-19 19:14:24 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,8 +5,8 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID === undefined? "G75RVLCR#fqKH6m2TkHsoHnb-c7VEo1IHl-gie2TP1p_xXGQhUak":process.env.SESSION_ID,
-MONGODB: process.env.MONGODB || "mongodb://mongo:zATSetwQySOzzuYMHcxPhjWrAnoKusUA@shortline.proxy.rlwy.net:20703",
+SESSION_ID: process.env.SESSION_ID === undefined? "ENTER YOUR SESSION ID":process.env.SESSION_ID,
+MONGODB: process.env.MONGODB || "ENTER YOUR MONGO DB URL",
 GEMINI_API_KEY: process.env.GEMINI_API_KEY || "AIzaSyDzyQ7tnQkquWX8bc1y9TE0auCHbC_QQO8",
 tokenm: process.env.GITHUB_TOKEN || "false",
 repo: process.env.GITHUB_REPO || "RAWANA-MD",
